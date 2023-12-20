81d83332c9df0f0ae700764072461f171baad360
TLTOscar <105489320+TLTOscar@users.noreply.github.com>
2023-12-20 08:38:23 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cfvek2vobtt4.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tangjianxian-employee"
+customregion = "us-east-1"
+
